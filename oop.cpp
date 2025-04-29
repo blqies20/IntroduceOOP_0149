8e036f70117f01e0bd1b0d0a2adacaedf8ada93e
blqies20 <balqis1maharani@gmail.com>
2025-04-29 10:42:20 +0700

    menambahkan member fungsi print data

--- oop.cpp
+++ oop.cpp
@@ -7,6 +7,11 @@ public:
     int nim;
     int nama;
     float nilai;
+    void printData(){
+        cout<<"NIM \t= "<<nim<<endl;
+        cout<<"Nama \t= "<<nama<<endl;
+        cout<<"Nilai \t= "<<nilai<<endl;
+    }
 };
 
 int main()
